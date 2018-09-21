a2229de1353a30e058bf6561d44067eba7a38e17
Ricardo Cruz <piradoiv@gmail.com>
2018-09-21 16:55:30 +0100

    Remove old tests

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-clear
-rm -f tests/PrackTests
-echo -en "Building tests...\n\n"
-lazbuild -q -q tests/PrackTests.lpi
-echo -en "\nRunning tests...\n\n"
-./tests/PrackTests -a --sparse --format=plain

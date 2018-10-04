7d269513787cecfdf7f4e44b4f17a115ea81ee12
Ricardo Cruz <piradoiv@gmail.com>
2018-10-04 20:11:33 +0100

    Move ProcessPendingRequests to its own procedure

--- src/Orchestra.pas
+++ src/Orchestra.pas
@@ -20,6 +20,7 @@ type
     FQueue: TPrackQueue;
   protected
     procedure Execute; override;
+    procedure ProcessPendingRequests(List: TList);
   public
     constructor Create(aQueue: TPrackQueue);
   end;
@@ -37,37 +38,44 @@ end;
 procedure TOrchestra.Execute;
 var
   List: TList;
-  Connection: TPrackConnection;
-  I: integer;
 begin
   while not Terminated do
   begin
     FQueue.ReadyRequestsEvent.WaitFor(READY_REQUEST_LIMIT);
     List := FQueue.LockList;
     try
-      for I := List.Count - 1 downto 0 do
-      begin
-        Connection := TPrackConnection(List.Items[I]);
-        Connection.Setup;
-
-        if Connection.Status = pcsError then
-          Connection.SetErrorResponse(502, 'Backend error' + CRLF);
-
-        if (Connection.Status <> pcsReady) and
-          (SecondsBetween(Now, Connection.CreatedAt) >= TIMEOUT_LIMIT) then
-          Connection.SetErrorResponse(504, 'Timeout' + CRLF);
-
-        if Connection.Status <> pcsReady then
-          Continue;
-
-        // TODO: Responses can be sent in parallel
-        Connection.SendResponse;
-        List.Remove(Connection);
-        FreeAndNil(Connection);
-      end;
+      ProcessPendingRequests(List);
     finally
       FQueue.UnlockList;
     end;
+  end;
+end;
+
+procedure TOrchestra.ProcessPendingRequests(List: TList);
+var
+  Connection: TPrackConnection;
+  I: integer;
+begin
+  for I := List.Count - 1 downto 0 do
+  begin
+    Connection := TPrackConnection(List.Items[I]);
+    Connection.Setup;
+
+    if Connection.Status = pcsError then
+      Connection.SetErrorResponse(502, 'Backend error' + CRLF);
+
+    if (Connection.Status <> pcsReady) and
+      (SecondsBetween(Now, Connection.CreatedAt) >= TIMEOUT_LIMIT) then
+      Connection.SetErrorResponse(504, 'Timeout' + CRLF);
+
+    if Connection.Status <> pcsReady then
+      Continue;
+
+    // TODO: Responses can be sent in parallel
+    // https://github.com/piradoiv/Prack/issues/2
+    Connection.SendResponse;
+    List.Remove(Connection);
+    FreeAndNil(Connection);
   end;
 end;
 
